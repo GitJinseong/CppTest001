39b1d3bf85f50c7884fd60dc3ff0a4ccf1bb773d
DESKTOP-QO8IJTE\KGA <kcgjinseong@gmail.com>
2023-05-31 15:51:57 +0900

    Update FourthProject

--- FourthProject/FourthProject/FourthProject.cpp
+++ FourthProject/FourthProject/FourthProject.cpp
@@ -55,6 +55,19 @@ int main()
 
     result1 = result1 + (num1 + num2); // 복합 연산자 사용 전
     result1 += (num1 + num2); // 복합 연산자 사용 후
+    
+    printf("num1 : %d \n", ++num1);// + 전위 증감 연산자
+    printf("num1 : %d \n", num1++);// + 후위 증감 연산자
+                                   // printf 출력 후에 num1을 증감한다.
+
+    printf("num1 : %d \n", --num1); // + 전위 감산 연산자
+    printf("num1 : %d \n\n", num1--); // + 후위 감산 연산자
+                                   // printf 출력 후에 num1을 감산한다.
+
+    int someNumber = 0;
+    someNumber = someNumber++; // 후위 연산자를 지나간 후에 someNumber을 증감시킨다.
+
+ printf("Some Number 안에는 무슨 값이 들어 있을까? %d \n\n", someNumber); someNumber++;
 
     printf("%d + %d = %d \n", num1, num2, num1 + num2); // 더하기
     printf("%d - %d = %d \n", num1, num2, num1 - num2); // 빼기
@@ -69,6 +82,19 @@ int main()
     printf("%d + %d = %d \n\n", num1, num2, REST(num1, num2)); // 나머지(mod연산)
 
     printf("현재 HP = %d \n\n", RECOVERY_ONE_S_PHYSICAL_STRENGTH(50, 100, 20)); // 최대체력, 힐량, 현재체력
+
+    int num3 = 10;
+    int num4 = 12;
+    int result2, result3, result4;
+
+    result2 = num3 == num4; // 거짓이므로 0을 반환
+    result3 = num3 <= num4; // 참이므로 1을 반환
+    result4 = num3 > num4; // 거짓이므로 0을 반환
+
+    printf("Result1 결과는 %d \n", result2);
+    printf("Result2 결과는 %d \n", result3);
+    printf("Result3 결과는 %d \n", result4);
+
 }
 
 
