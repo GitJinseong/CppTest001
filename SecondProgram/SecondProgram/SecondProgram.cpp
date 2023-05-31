400dbf57b079044d7cbfe6b770c74841d0ca944a
DESKTOP-QO8IJTE\KGA <kcgjinseong@gmail.com>
2023-05-31 12:10:56 +0900

    Upload New Files

--- SecondProgram/SecondProgram/SecondProgram.cpp
+++ SecondProgram/SecondProgram/SecondProgram.cpp
@@ -1,6 +1,4 @@
-﻿// SecondProgram.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿
 #include <iostream>
 
 //이런게 함수의 정의다.
@@ -10,10 +8,22 @@ int PRS_INF_PRN(int age)
 	return 0;
 }
 
-int main()
+int main() //여기서 프로그램 실행이 시작되고 종료된다.
 {
+
+	int number = 10;	// integer가 정수다. 그러니 정수를 저장하기 위한
+						// 데이터 타입으로 줄임말인 int를 썻다.
+
+	int number2 = 1.6; // int에 소수점을 입력하면 출력시 소수점이 나오지 않는다. 
+	float number3 = 1.6;	// float는 실수다. 
+
 	printf("이게 처음 호출된다. \n");
+	printf("%d \n", number2);	// int는 %d를 사용한다.
+	printf("%f \n", number3);	// float는 %f를 사용한다.
+
 	// 중요한 거, C++에서 main 함수는 무조건 있어야 한다.
 	// 프로그램은 main 함수에서부터 시작한다.
+
 	PRS_INF_PRN(27);		//이런게 함수의 호출이다.
+
 }
---
+++ ThirdProject/ThirdProject/ThirdProject.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+
+void CRITICAL_HIT_DAMAGE_PRN(int damage)
+{
+    float critical = damage * 1.5;
+ 
+    srand(time(NULL));  // 난수 초기화
+    int probabilitySuccess = rand() % 3+1; // 0~3 사이의 랜덤 값을 설정
+
+    if (probabilitySuccess <= 2) // 나온 값이 2 이상시 성공(50%)
+            printf("데미지 %f를 가했습니다.", critical);
+        else
+            printf("실패했습니다.");
+
+}
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+    CRITICAL_HIT_DAMAGE_PRN(100);
+
+}
