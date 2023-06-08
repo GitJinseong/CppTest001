3f1a9aad5522700c8c93ec802376759e4a038b6e
DESKTOP-QO8IJTE\KGA <kcgjinseong@gmail.com>
2023-06-08 17:59:10 +0900

    upload files pm 05:59

---
+++ 49stProject/49stProject/49stProject.cpp
@@ -0,0 +1,30 @@
+﻿// 49stProject.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+void StartSlidingGame(int size);
+
+int main()
+{
+    StartSlidingGame(3);
+}
+
+// [과제]
+// 슬라이딩 퍼즐게임 구현해서 제출하기
+// 3 ~ 6 사이의 값을 입력받아서 해당 사이즈에 맞는 슬라이딩 퍼즐 구현하기(Normal)
+// ex) 3 x 3 = ㅁㅁㅁ
+//             ㅁㅁㅁ
+//             ㅁㅁ 
+// 게임 배열이 오름차순으로 정렬될 경우 게임 클리어.
+// 클리어가 불가능한 퍼즐은 존재하지 않도록 구현할 것(Very Hard)
+void StartSlidingGame(int size)
+{
+
+}
